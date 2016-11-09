63a604a7e4dce55ea641b7506fe027c263995f2d
Julius Haertl <jus@bitgrid.net>
2016-11-09 14:00:45 +0100

    Reformat code

--- lib/Controller/ShareController.php
+++ lib/Controller/ShareController.php
@@ -5,20 +5,20 @@
  * @author Julius Härtl <jus@bitgrid.net>
  *
  * @license GNU AGPL version 3 or any later version
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
  *  published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ *
  */
 
 namespace OCA\Deck\Controller;
@@ -33,25 +33,25 @@ use OCP\IUserManager;
 
 class ShareController extends Controller {
 
-    private $userManager;
-    private $groupManager;
+	private $userManager;
+	private $groupManager;
 	private $boardService;
-    private $userId;
+	private $userId;
 
-    public function __construct($appName,
-                                IRequest $request,
-                                IUserManager $userManager,
-                                IGroupManager $groupManager,
+	public function __construct($appName,
+								IRequest $request,
+								IUserManager $userManager,
+								IGroupManager $groupManager,
 								BoardService $boardService,
-                                $userId
-    ){
-        parent::__construct($appName, $request);
-        $this->userManager = $userManager;
-        $this->groupManager = $groupManager;
-        $this->userId = $userId;
+								$userId
+	) {
+		parent::__construct($appName, $request);
+		$this->userManager = $userManager;
+		$this->groupManager = $groupManager;
+		$this->userId = $userId;
 		$this->boardService = $boardService;
 
-    }
+	}
 
 	/**
 	 * @NoAdminRequired
@@ -59,35 +59,33 @@ class ShareController extends Controller {
 	 * @param $search
 	 * @return array
 	 */
-    public function searchUser($search) {
-        $limit = 3;
-        $offset = null;
-        $result = [];
-        foreach ($this->groupManager->search($search, $limit, $offset) as $idx => $group) {
-            $acl = new Acl();
-            $acl->setType('group');
-            $acl->setParticipant($group->getGID());
-            $acl->setPermissionWrite(true);
-            $acl->setPermissionInvite(true);
-            $acl->setPermissionManage(true);
-            $result[] = $acl;
-        }
-        $limit = 10;
-        foreach ($this->userManager->searchDisplayName($search, $limit, $offset) as $idx => $user) {
-            if($user->getUID() === $this->userId)
-                continue;
-            $acl = new Acl();
-            $acl->setType('user');
-            $acl->setParticipant($user->getUID());
-            $acl->setPermissionWrite(true);
-            $acl->setPermissionInvite(true);
-            $acl->setPermissionManage(true);
-            $result[] = $acl;
-        }
-        return $result;
-    }
-
-
+	public function searchUser($search) {
+		$limit = 3;
+		$offset = null;
+		$result = [];
+		foreach ($this->groupManager->search($search, $limit, $offset) as $idx => $group) {
+			$acl = new Acl();
+			$acl->setType('group');
+			$acl->setParticipant($group->getGID());
+			$acl->setPermissionWrite(true);
+			$acl->setPermissionInvite(true);
+			$acl->setPermissionManage(true);
+			$result[] = $acl;
+		}
+		$limit = 10;
+		foreach ($this->userManager->searchDisplayName($search, $limit, $offset) as $idx => $user) {
+			if ($user->getUID() === $this->userId)
+				continue;
+			$acl = new Acl();
+			$acl->setType('user');
+			$acl->setParticipant($user->getUID());
+			$acl->setPermissionWrite(true);
+			$acl->setPermissionInvite(true);
+			$acl->setPermissionManage(true);
+			$result[] = $acl;
+		}
+		return $result;
+	}
 
 
 }
