d74b9f68d9fac203b307a50648a7857d3a1d8f70
Julius Haertl <jus@bitgrid.net>
2017-02-06 22:49:43 +0100

    Use select2 styles from Nextcloud server

--- templates/part.board.sidebarView.php
+++ templates/part.board.sidebarView.php
@@ -18,14 +18,14 @@
     <div id="commentsTabView" class="tab commentsTabView" ng-if="status.boardtab==0 || !status.boardtab">
 
         <ui-select ng-if="boardservice.canShare()" ng-model="status.addSharee" theme="select2" style="width:100%;" title="Choose a user to assign" placeholder="Assign users ..." on-select="aclAdd(status.addSharee)">
-            <ui-select-match placeholder="<?php p($l->t('Select users...')); ?>">
+            <ui-select-match placeholder="<?php p($l->t('Select users or groups to share with')); ?>">
                 <span><i class="icon icon-{{$item.type}}"></i> {{ $item.participant }}</span>
             </ui-select-match>
             <ui-select-choices refresh="searchForUser($select.search)" refresh-delay="0" repeat="sharee in boardservice.sharees">
                 <span><i class="icon icon-{{sharee.type}}"></i> {{ sharee.participant }}</span>
             </ui-select-choices>
             <ui-select-no-choice>
-            <?php p($l->t('No users found.')); ?>
+            <?php p($l->t('No matching user or group found.')); ?>
             </ui-select-no-choice>
         </ui-select>
 
@@ -34,8 +34,8 @@
                 <span class="icon-loading-small" style="display:none;"></span>
                 <div class="avatardiv" avatar ng-attr-displayname="{{ boardservice.getCurrent().owner }}" ng-if="boardservice.id"></div>
                 <span class="has-tooltip username">
-                    {{ boardservice.getCurrent().owner }}</span>
-                <span class="shareOption"><?php p($l->t('Board owner')); ?></span>
+                    {{ boardservice.getCurrent().owner }}
+				</span>
             </li>
             <li ng-repeat="acl in boardservice.getCurrent().acl track by $index">
                 <span class="icon-loading-small" style="display:none;"></span>
@@ -43,7 +43,9 @@
                 <div class="avatardiv" ng-if="acl.type=='group'"><i class="icon icon-{{acl.type}}"></i></div>
 
                 <span class="has-tooltip username">
-                    {{ acl.participant }}</span>
+                    {{ acl.participant }}
+				</span>
+				<span class="sharingOptionsGroup">
                 <span class="shareOption" ng-if="boardservice.canManage()">
                     <input type="checkbox" class="permissions checkbox" id="checkbox-permission-{{ acl.id }}-share" ng-model="acl.permissionShare" ng-change="aclUpdate(acl)" />
                     <label for="checkbox-permission-{{ acl.id }}-share"><?php p($l->t('Share')); ?></label>
@@ -56,7 +58,8 @@
                     <input type="checkbox" class="permissions checkbox" id="checkbox-permission-{{ acl.id }}-manage" ng-model="acl.permissionManage" ng-change="aclUpdate(acl)" />
                     <label for="checkbox-permission-{{ acl.id }}-manage"><?php p($l->t('Manage')); ?></label>
                 </span>
-                <a ng-if="boardservice.canManage()" class="unshare" ng-click="aclDelete(acl)"><span class="icon-loading-small hidden"></span><span class="icon icon-delete"></span><span class="hidden-visually"><?php p($l->t('Discard share')); ?></span></a>
+				</span>
+                <a ng-if="boardservice.canManage()" ng-click="aclDelete(acl)"><span class="icon-loading-small hidden"></span><span class="icon icon-delete"></span><span class="hidden-visually"><?php p($l->t('Discard share')); ?></span></a>
             </li>
         </ul>
 
