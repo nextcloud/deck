b19b1fd78b459817e1b6c133948ce41b6874d7a1
Marin Treselj <marin@pixelipo.com>
2017-05-09 08:41:35 +0200

    Move new-stack input to the right

--- templates/part.board.mainView.php
+++ templates/part.board.mainView.php
@@ -11,16 +11,6 @@
 	<h1 class="title" style="border-bottom: 2px solid #{{boardservice.getCurrent().color }};">
 		{{ boardservice.getCurrent().title }}
 	</h1>
-	<div id="stack-add" ng-if="boardservice.canEdit() && checkCanEdit()">
-		<form class="ng-pristine ng-valid" ng-submit="createStack()">
-			<input type="text" placeholder="Add a new stack"
-				ng-focus="status.addStack=true"
-				ng-blur="status.addStack=false"
-				ng-model="newStack.title" required
-				maxlength="100" />
-			<button class="icon icon-add" style="opacity: {{status.addStack ? 1: 0.5}};" type="submit"></button>
-		</form>
-	</div>
 	<div class="board-action-button">
 		<a ui-sref="board.detail({ id: id })" title="<?php p($l->t('Board details')); ?>">
 			<i class="icon icon-details"></i>
@@ -35,6 +25,16 @@
 		<a ng-click="switchFilter('')" title="<?php p($l->t('Hide archived cards')); ?>">
 			<i class="icon icon-archive"></i>
 		</a>
+	</div>
+	<div id="stack-add" class="board-action-button" ng-if="boardservice.canEdit() && checkCanEdit()">
+		<form class="ng-pristine ng-valid" ng-submit="createStack()">
+			<input type="text" placeholder="Add a new stack"
+				ng-focus="status.addStack=true"
+				ng-blur="status.addStack=false"
+				ng-model="newStack.title" required
+				maxlength="100" />
+			<button class="icon icon-add" style="opacity: {{status.addStack ? 1: 0.5}};" type="submit"></button>
+		</form>
 	</div>
 </div>
 
