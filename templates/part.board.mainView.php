be75d28170a5c22ffdd4e05264be304885ea7ce8
Marin Treselj <marin@pixelipo.com>
2017-05-10 14:44:05 +0200

    Header redesign - fixes #116
    Signed-off-by: Marin Treselj <marin@pixelipo.com>

--- templates/part.board.mainView.php
+++ templates/part.board.mainView.php
@@ -4,30 +4,40 @@
 		<h2>{{ statusservice.title }}</h2>
 		<p>{{ statusservice.text }}</p></div>
 </div>
-<div id="board-header"
-	 style="background-color: #{{boardservice.getCurrent().color }}; color: {{boardservice.getCurrent().color | textColorFilter }};">
-	<h1>
+<div id="board-header">
+	<a href="#" title="<?php p($l->t('All Boards')); ?>">
+		<i class="icon icon-home"></i>
+	</a>
+	<h1 class="title" style="border-bottom: 2px solid #{{boardservice.getCurrent().color }};">
 		{{ boardservice.getCurrent().title }}
 	</h1>
-  	<div id="board-actions">
-		<div class="board-action-button" ng-if="filter!='archive'"><a ng-click="switchFilter('archive')" style="opacity:0.5;" title="<?php p($l->t('Show archived cards')); ?>"><i class="icon icon-archive{{ boardservice.getCurrent().color | iconWhiteFilter }}"></i></a></div>
-		<div class="board-action-button" ng-if="filter=='archive'"><a ng-click="switchFilter('')" title="<?php p($l->t('Hide archived cards')); ?>"><i class="icon icon-archive{{ boardservice.getCurrent().color | iconWhiteFilter }}"></i></a></div>
-		<div class="board-action-button"><a ui-sref="board.detail({ id: id })" title="<?php p($l->t('Board details')); ?>"><i class="icon icon-details{{ boardservice.getCurrent().color | iconWhiteFilter }}"></i></a>
-    </div>
-</div>
-
-<div id="stack-add" ng-if="boardservice.canEdit() && checkCanEdit()">
-	<form class="ng-pristine ng-valid" ng-submit="createStack()">
-		<input type="text" placeholder="Add a new stack"
+	<div class="board-action-button">
+		<a ui-sref="board.detail({ id: id })" title="<?php p($l->t('Board details')); ?>">
+			<i class="icon icon-details"></i>
+		</a>
+	</div>
+	<div class="board-action-button" ng-if="filter!='archive'">
+		<a ng-click="switchFilter('archive')" style="opacity:0.5;" title="<?php p($l->t('Show archived cards')); ?>">
+			<i class="icon icon-archive"></i>
+		</a>
+	</div>
+	<div class="board-action-button" ng-if="filter=='archive'">
+		<a ng-click="switchFilter('')" title="<?php p($l->t('Hide archived cards')); ?>">
+			<i class="icon icon-archive"></i>
+		</a>
+	</div>
+	<div id="stack-add" class="board-action-button" ng-if="boardservice.canEdit() && checkCanEdit()">
+		<form class="ng-pristine ng-valid" ng-submit="createStack()">
+			<input type="text" placeholder="Add a new stack"
 				ng-focus="status.addStack=true"
 				ng-blur="status.addStack=false"
 				ng-model="newStack.title" required
 				maxlength="100" />
-		<button class="icon icon-add" style="opacity: {{status.addStack ? 1: 0.5}};"
-				type="submit"></button>
-	</form>
+			<button class="icon icon-add" style="opacity: {{status.addStack ? 1: 0.5}};" type="submit"></button>
+		</form>
 	</div>
 </div>
+
 <div id="board" class="scroll-container" ng-click="sidebar.show=false" ui-sref="board">
 
 	<search on-search="search" class="ng-hide"></search>
@@ -126,7 +136,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 	</div>
 
 </div>
