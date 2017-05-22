1e4e0a39478cb03ac2d380b1ae5a078693532cbe
Artem Anufrij <artem.anufrij@live.de>
2017-05-22 19:40:07 +0200

    added home button
    Signed-off-by: Artem Anufrij <artem.anufrij@live.de>

--- templates/part.board.mainView.php
+++ templates/part.board.mainView.php
@@ -5,9 +5,11 @@
 		<p>{{ statusservice.text }}</p></div>
 </div>
 <div id="board-header">
-	<a href="#"  class="crumb svg last" id="home" title="<?php p($l->t('All Boards')); ?>">
-		<img class="svg" src="../../../core/img/places/home.svg" alt="Home">
-	</a>
+	<div id="home" class="crumb svg last">
+		<a href="#" title="<?php p($l->t('All Boards')); ?>">
+			<img class="svg" src="../../../core/img/places/home.svg" alt="Home">
+		</a>
+	</div>
 	<h1 class="title" style="border-bottom: 2px solid #{{boardservice.getCurrent().color }};">
 		{{ boardservice.getCurrent().title }}
 	</h1>
