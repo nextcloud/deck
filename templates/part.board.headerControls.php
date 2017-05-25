1defc4817980bc156b994bd83a3887dafbcd846c
Marin Treselj <marin@pixelipo.com>
2017-05-25 14:57:11 +0200

    This is part one of the style cleanup mentioned in #160.
    Functionality has not been affected (I hope). There will be few more related PRs but I want to keep each one simple so it's easy to review.
    
    Signed-off-by: Marin Treselj <marin@pixelipo.com>

---
+++ templates/part.board.headerControls.php
@@ -0,0 +1,20 @@
+<div id="stack-add" ng-if="boardservice.canEdit() && checkCanEdit()">
+    <form class="ng-pristine ng-valid" ng-submit="createStack()">
+        <input type="text" class="no-close" placeholder="Add a new stack"
+            ng-focus="status.addStack=true"
+            ng-blur="status.addStack=false"
+            ng-model="newStack.title" required
+            maxlength="100" />
+        <button class="button-inline icon icon-add" style="opacity: {{status.addStack ? 1: 0.5}};" type="submit"></button>
+    </form>
+</div>
+
+<a class="board-action-button button" ng-if="filter!='archive'" ng-click="switchFilter('archive')" style="opacity:0.5;" title="<?php p($l->t('Show archived cards')); ?>">
+    <i class="icon icon-archive"></i>
+</a>
+<a class="board-action-button button" ng-if="filter=='archive'" ng-click="switchFilter('')" title="<?php p($l->t('Hide archived cards')); ?>">
+    <i class="icon icon-archive"></i>
+</a>
+<a class="board-action-button button" ui-sref="board.detail({ id: id })"  title="<?php p($l->t('Board details')); ?>">
+    <i class="icon icon-details"></i>
+</a>
