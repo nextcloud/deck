e06a2ad743d988aa5ccd7f8d22ec5c8bf6823eaa
Marin Treselj <marin@pixelipo.com>
2017-05-31 08:17:18 +0200

    fix tabs
    Signed-off-by: Marin Treselj <marin@pixelipo.com>

--- templates/part.board.mainView.php
+++ templates/part.board.mainView.php
@@ -35,7 +35,7 @@
 			 data-columnindex="{{$index}}" id="column{{$index}}"
 			 style="">
 			<h2 data-as-sortable-item-handle>
-                <span ng-show="!s.status.editStack" ng-click="s.status.editStack=true">{{ s.title }}</span>
+				<span ng-show="!s.status.editStack" ng-click="s.status.editStack=true">{{ s.title }}</span>
 				<form ng-if="s.status.editStack" ng-submit="stackservice.update(s)">
 					<input type="text" placeholder="Add a new stack"
 						   ng-blur="stackservice.update(s); s.status.editStack=false" ng-model="s.title"
