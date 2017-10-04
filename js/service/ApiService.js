320f2bf5c81aefc6f59a374c1083f948386a50a1
Julius Härtl <jus@bitgrid.net>
2017-10-04 15:38:08 +0200

    JS: Fix scrutinizer warnings and indentation for services
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/service/ApiService.js
+++ js/service/ApiService.js
@@ -20,152 +20,152 @@
  *  
  */
 
-app.factory('ApiService', function($http, $q){
-    var ApiService = function(http, endpoint) {
-        this.endpoint = endpoint;
-        this.baseUrl = OC.generateUrl('/apps/deck/' + endpoint);
-        this.http = http;
-        this.q = $q;
-        this.data = {};
-        this.id = null;
-        this.sorted = [];
-    };
+/** global: oc_defaults */
+app.factory('ApiService', function ($http, $q) {
+	var ApiService = function (http, endpoint) {
+		this.endpoint = endpoint;
+		this.baseUrl = OC.generateUrl('/apps/deck/' + endpoint);
+		this.http = http;
+		this.q = $q;
+		this.data = {};
+		this.id = null;
+		this.sorted = [];
+	};
 
-    ApiService.prototype.fetchAll = function(){
-        var deferred = $q.defer();
-        var self = this;
-        $http.get(this.baseUrl).then(function (response) {
-            var objects = response.data;
-            objects.forEach(function (obj) {
-                self.data[obj.id] = obj;
-            });
-            deferred.resolve(self.data);
-        }, function (error) {
-            deferred.reject('Fetching ' + self.endpoint + ' failed');
-        });
-        return deferred.promise;
-    };
+	ApiService.prototype.fetchAll = function () {
+		var deferred = $q.defer();
+		var self = this;
+		$http.get(this.baseUrl).then(function (response) {
+			var objects = response.data;
+			objects.forEach(function (obj) {
+				self.data[obj.id] = obj;
+			});
+			deferred.resolve(self.data);
+		}, function (error) {
+			deferred.reject('Fetching ' + self.endpoint + ' failed');
+		});
+		return deferred.promise;
+	};
 
-    ApiService.prototype.fetchOne = function (id) {
+	ApiService.prototype.fetchOne = function (id) {
 
-        this.id = id;
-        var deferred = $q.defer();
+		this.id = id;
+		var deferred = $q.defer();
 
-        if(id===undefined) {
-            return deferred.promise;
-        }
+		if (id === undefined) {
+			return deferred.promise;
+		}
 
-        var self = this;
-        $http.get(this.baseUrl + '/' + id).then(function (response) {
-            data = response.data;
-            if(self.data[data.id]===undefined) {
-                self.data[data.id] = response.data;
-            }
-            $.each(response.data, function(key, value) {
-                self.data[data.id][key] = value;
-            });
-            deferred.resolve(response.data);
+		var self = this;
+		$http.get(this.baseUrl + '/' + id).then(function (response) {
+			var data = response.data;
+			if (self.data[data.id] === undefined) {
+				self.data[data.id] = response.data;
+			}
+			$.each(response.data, function (key, value) {
+				self.data[data.id][key] = value;
+			});
+			deferred.resolve(response.data);
 
-        }, function (error) {
-            deferred.reject('Fetching ' + self.endpoint + ' failed');
-        });
-        return deferred.promise;
-    };
+		}, function (error) {
+			deferred.reject('Fetching ' + self.endpoint + ' failed');
+		});
+		return deferred.promise;
+	};
 
-    ApiService.prototype.create = function (entity) {
-        var deferred = $q.defer();
-        var self = this;
-        $http.post(this.baseUrl, entity).then(function (response) {
-            self.add(response.data);
-            deferred.resolve(response.data);
-        }, function (error) {
-            deferred.reject('Fetching' + self.endpoint + ' failed');
-        });
-        return deferred.promise;
-    };
+	ApiService.prototype.create = function (entity) {
+		var deferred = $q.defer();
+		var self = this;
+		$http.post(this.baseUrl, entity).then(function (response) {
+			self.add(response.data);
+			deferred.resolve(response.data);
+		}, function (error) {
+			deferred.reject('Fetching' + self.endpoint + ' failed');
+		});
+		return deferred.promise;
+	};
 
-    ApiService.prototype.update = function (entity) {
-        var deferred = $q.defer();
-        var self = this;
-        $http.put(this.baseUrl + '/' + entity.id, entity).then(function (response) {
-            self.add(response.data);
-            deferred.resolve(response.data);
-        }, function (error) {
-            deferred.reject('Updating ' + self.endpoint + ' failed');
-        });
-        return deferred.promise;
+	ApiService.prototype.update = function (entity) {
+		var deferred = $q.defer();
+		var self = this;
+		$http.put(this.baseUrl + '/' + entity.id, entity).then(function (response) {
+			self.add(response.data);
+			deferred.resolve(response.data);
+		}, function (error) {
+			deferred.reject('Updating ' + self.endpoint + ' failed');
+		});
+		return deferred.promise;
 
-    };
+	};
 
-    ApiService.prototype.delete = function (id) {
-        var deferred = $q.defer();
-        var self = this;
+	ApiService.prototype.delete = function (id) {
+		var deferred = $q.defer();
+		var self = this;
 
-        $http.delete(this.baseUrl + '/' + id).then(function (response) {
-            self.remove(id);
-            deferred.resolve(response.data);
+		$http.delete(this.baseUrl + '/' + id).then(function (response) {
+			self.remove(id);
+			deferred.resolve(response.data);
 
-        }, function (error) {
-            deferred.reject('Deleting ' + self.endpoint + ' failed');
-        });
-        return deferred.promise;
+		}, function (error) {
+			deferred.reject('Deleting ' + self.endpoint + ' failed');
+		});
+		return deferred.promise;
 
-    };
-    
+	};
 
-    // methods for managing data
-    ApiService.prototype.clear = function() {
-        this.data = {};
-    };
-    ApiService.prototype.add = function (entity) {
-        var element = this.data[entity.id];
-        if(element===undefined) {
-            this.data[entity.id] = entity;
-        } else {
-            Object.keys(entity).forEach(function (key) {
-                element[key] = entity[key];
-            });
-            element.status = {};
-        }
-    };
-    ApiService.prototype.remove = function(id) {
-        if (this.data[id] !== undefined) {
-            delete this.data[id];
-        }
-    };
-    ApiService.prototype.addAll = function (entities) {
-        var self = this;
-        angular.forEach(entities, function(entity) {
-            self.add(entity);
-        });
-    };
 
-    ApiService.prototype.getCurrent = function () {
-        return this.data[this.id];
-    };
+	// methods for managing data
+	ApiService.prototype.clear = function () {
+		this.data = {};
+	};
+	ApiService.prototype.add = function (entity) {
+		var element = this.data[entity.id];
+		if (element === undefined) {
+			this.data[entity.id] = entity;
+		} else {
+			Object.keys(entity).forEach(function (key) {
+				element[key] = entity[key];
+			});
+			element.status = {};
+		}
+	};
+	ApiService.prototype.remove = function (id) {
+		if (this.data[id] !== undefined) {
+			delete this.data[id];
+		}
+	};
+	ApiService.prototype.addAll = function (entities) {
+		var self = this;
+		angular.forEach(entities, function (entity) {
+			self.add(entity);
+		});
+	};
+
+	ApiService.prototype.getCurrent = function () {
+		return this.data[this.id];
+	};
 
 	ApiService.prototype.unsetCurrrent = function () {
 		this.id = null;
 	};
 
 
+	ApiService.prototype.getData = function () {
+		return $.map(this.data, function (value, index) {
+			return [value];
+		});
+	};
 
-	ApiService.prototype.getData = function() {
-        return $.map(this.data, function(value, index) {
-            return [value];
-        });
-    };
+	ApiService.prototype.getAll = function () {
+		return this.data;
+	};
 
-    ApiService.prototype.getAll = function () {
-        return this.data;
-    };
+	ApiService.prototype.getName = function () {
+		var funcNameRegex = /function (.{1,})\(/;
+		var results = (funcNameRegex).exec((this).constructor.toString());
+		return (results && results.length > 1) ? results[1] : "";
+	};
 
-    ApiService.prototype.getName = function() {
-        var funcNameRegex = /function (.{1,})\(/;
-        var results = (funcNameRegex).exec((this).constructor.toString());
-        return (results && results.length > 1) ? results[1] : "";
-    };
-
-    return ApiService;
+	return ApiService;
 
 });
