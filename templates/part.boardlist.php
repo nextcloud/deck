e1793b5c3155e4b0eaa569f790114e3bb3de0ead
Julius Härtl <jus@bitgrid.net>
2017-10-26 11:19:46 +0200

    Performance optimizations in list template
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- templates/part.boardlist.php
+++ templates/part.boardlist.php
@@ -26,16 +26,15 @@
 		<tbody>
 		<tr data-ng-repeat="b in boardservice.sorted" ng-class="{deleted: b.deletedAt > 0}">
 			<td ng-click="gotoBoard(b)">
-				<div class="board-bullet"
-					  ng-style="{'background-color':'#{{b.color}}'};"> </div>
+				<div class="board-bullet" ng-style="{'background-color':'#'+b.color}"> </div>
 			</td>
 			<td>
 				<div ng-click="gotoBoard(b)" ng-show="!b.status.edit">{{ b.title }}</div>
 				<div class="app-navigation-entry-edit" ng-show="b.status.edit">
 					<form ng-disabled="isAddingList" class="ng-pristine ng-valid" ng-submit="boardUpdate(b)">
-						<input id="newTitle" class="edit ng-valid ng-empty" type="text" autofocus-on-insert ng-model="b.title" maxlength="100">
+						<input id="newTitle" class="edit ng-valid ng-empty" type="text" autofocus-on-insert ng-model="b.title" maxlength="100" ng-model-options="{ debounce: 250 }">
 						<div class="colorselect">
-							<div class="color" ng-repeat="c in colors" ng-style="{'background-color':'#{{ c }}'}" ng-click="b.color=c" ng-class="{'selected': (c == b.color) }"><br /></div>
+							<div class="color" ng-repeat="c in ::colors" ng-style="{'background-color':'#{{ c }}'}" ng-click="b.color=c" ng-class="{'selected': (c == b.color) }"><br /></div>
 						</div>
 					</form>
 				</div>
@@ -101,9 +100,9 @@
 					  class="ng-pristine ng-valid" ng-submit="boardCreate()">
 					<input id="newTitle" class="edit ng-valid ng-empty"
 						   type="text" placeholder="<?php p($l->t('New board title')); ?>"
-						   autofocus-on-insert ng-model="newBoard.title" maxlength="100">
+						   autofocus-on-insert ng-model="newBoard.title" maxlength="100" ng-model-options="{ debounce: 250 }">
 					<div class="colorselect">
-						<div class="color" ng-repeat="c in colors"
+						<div class="color" ng-repeat="c in ::colors"
 							 ng-style="{'background-color':'#{{ c }}'}"
 							 ng-click="selectColor(c)"
 							 ng-class="{'selected': (c == newBoard.color), 'dark': (newBoard.color | textColorFilter) === '#ffffff' }"></div>
