0c0910ee8146a1a04afce5136c6a5b114e5b210b
Ryan Fletcher <ryan.fletcher@codepassion.ca>
2018-07-12 23:11:52 -0400

    Co authored for my changes.
    Signed-off-by: Ryan Fletcher <ryan.fletcher@codepassion.ca>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,6 +3,7 @@
  * @copyright Copyright (c) 2016 Julius Härtl <jus@bitgrid.net>
  *
  * @author Julius Härtl <jus@bitgrid.net>
+ * @author Ryan Fletcher <ryan.fletcher@codepassion.ca>
  *
  * @license GNU AGPL version 3 or any later version
  *
