a5a836450eea24197f764d6f3f19d80be10c0ddc
Julius Härtl <jus@bitgrid.net>
2018-10-09 11:55:00 +0200

    Bump babel to 7.x.x
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env'],
+	  presets: ['@babel/preset-env'],
         }
       },
       {
