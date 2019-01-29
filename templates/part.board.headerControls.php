ec72c7b1f1a4e1820c235b5ca0e2278f8c659431
Jakob Röhrl <jakob.roehrl@web.de>
2019-01-29 09:09:11 +0100

    now it
    Signed-off-by: Jakob Röhrl <jakob.roehrl@web.de>

--- templates/part.board.headerControls.php
+++ templates/part.board.headerControls.php
@@ -13,30 +13,19 @@
     </form>
 </div>
 
-<div class="section-label" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Show archived cards')); ?>">
-    <button ng-if="params.filter!='archive'" ng-click="switchFilter('archive')" title="<?php p($l->t('Show archived cards')); ?>">
-        <i class="icon icon-archive" style="opacity:0.5;"></i>
-        <span class="hidden-visually"><?php p($l->t('Show archived cards')); ?></span>
-    </button>
-</div>
-
-<div ng-if="params.filter=='archive'" class="section-label" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Hide archived cards')); ?>">
-    <button ng-click="switchFilter('')" title="<?php p($l->t('Hide archived cards')); ?>">
-        <i class="icon icon-archive"></i>
-        <span class="hidden-visually"><?php p($l->t('Hide archived cards')); ?></span>
-    </button>
-</div>
-
-<div class="section-label" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Toggle compact mode')); ?>">
-    <button ng-click="toggleCompactMode()" title="<?php p($l->t('Toggle compact mode')); ?>">
-        <i class="icon" ng-class="{ 'icon-toggle-compact-collapsed': compactMode, 'icon-toggle-compact-expanded': !compactMode }"></i>
-        <span class="hidden-visually"><?php p($l->t('Toggle compact mode')); ?></span>
-    </button>
-</div>
-
-<div class="section-label" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Show board details')); ?>">
-    <button ui-sref="board.detail({ id: id, tab: 0})"  title="<?php p($l->t('Show board details')); ?>">
-        <i class="icon icon-settings"></i>
-        <span class="hidden-visually"><?php p($l->t('Show board details')); ?></span>
-    </button>
-</div>
+<button ng-if="params.filter!='archive'" ng-click="switchFilter('archive')" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Show archived cards')); ?>">
+    <i class="icon icon-archive" style="opacity:0.5;"></i>
+    <span class="hidden-visually"><?php p($l->t('Show archived cards')); ?></span>
+</button>
+<button ng-if="params.filter=='archive'" ng-click="switchFilter('')" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Hide archived cards')); ?>">
+    <i class="icon icon-archive"></i>
+    <span class="hidden-visually"><?php p($l->t('Hide archived cards')); ?></span>
+</button>
+<button ng-click="toggleCompactMode()" data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Toggle compact mode')); ?>">
+	<i class="icon" ng-class="{ 'icon-toggle-compact-collapsed': compactMode, 'icon-toggle-compact-expanded': !compactMode }"></i>
+	<span class="hidden-visually"><?php p($l->t('Toggle compact mode')); ?></span>
+</button>
+<button ui-sref="board.detail({ id: id, tab: 0})"  data-toggle="tooltip" data-placement="bottom" title="<?php p($l->t('Show board details')); ?>">
+    <i class="icon icon-settings"></i>
+    <span class="hidden-visually"><?php p($l->t('Show board details')); ?></span>
+</button>
