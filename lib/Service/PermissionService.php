e7fcef3e3a989fcdf19dd4c070dfdbc4f438666c
Jakob <jakob.roehrl@web.de>
2019-10-15 08:30:25 +0200

    circle sharing
    Signed-off-by: Jakob <jakob.roehrl@web.de>

--- lib/Service/PermissionService.php
+++ lib/Service/PermissionService.php
@@ -262,6 +262,26 @@ class PermissionService {
 					$users[$user->getUID()] = new User($user);
 				}
 			}
+
+			if ($this->circlesEnabled && $acl->getType() === Acl::PERMISSION_TYPE_CIRCLE) {
+				try {
+					$circle = \OCA\Circles\Api\v1\Circles::detailsCircle($acl->getParticipant(), true);
+					if ($circle === null) {
+						$this->logger->info('No circle found for acl rule ' . $acl->getId());
+						continue;
+					}
+					
+					/* $b = $circle->getMember();
+
+					foreach ($circle['members'] as $user) {
+						$a = $user;
+						//$users[$user->userId()] = new User($user);
+					}  */ 
+					
+				} catch (\Exception $e) {
+					$this->logger->info('Member not found in circle that was accessed. This should not happen.');
+				}
+			}
 		}
 		$this->users[(string) $boardId] = $users;
 		return $this->users[(string) $boardId];
