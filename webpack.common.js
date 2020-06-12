6523328c9628084e85ccdb6f6376ffe78fdfec1d
Julius Härtl <jus@bitgrid.net>
2020-06-12 21:47:19 +0200

    Add contenthash to chunk filename
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,8 @@ module.exports = {
 		filename: '[name].js',
 		path: __dirname + '/js',
 		publicPath: '/js/',
-		jsonpFunction: 'webpackJsonpOCADeck'
+		jsonpFunction: 'webpackJsonpOCADeck',
+		chunkFilename: '[name].js?v=[contenthash]',
 	},
 	module: {
 		rules: [
