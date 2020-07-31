ccb09fdab8f38b24de4f6f48973bc8d0b0ee97f6
Julius Härtl <jus@bitgrid.net>
2020-07-31 21:30:39 +0200

    Use shared webpack config
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ webpack.js
@@ -0,0 +1,25 @@
+const webpackConfig = require('@nextcloud/webpack-vue-config')
+const path = require('path');
+const { merge } = require('webpack-merge')
+
+const config = {
+	entry: {
+		deck: path.join(__dirname, 'src', 'main.js'),
+		collections: path.join(__dirname, 'src', 'init-collections.js'),
+	},
+	output: {
+		filename: '[name].js',
+		jsonpFunction: 'webpackJsonpOCADeck',
+		chunkFilename: '[name].js?v=[contenthash]',
+	},
+	resolve: {
+		extensions: ['*', '.js', '.vue', '.json'],
+		modules: [
+			path.resolve(__dirname, 'node_modules'),
+			'node_modules'
+		]
+	}
+};
+
+module.exports = merge(config, webpackConfig)
+
