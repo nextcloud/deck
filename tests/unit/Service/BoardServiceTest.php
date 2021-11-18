5e31d44c99747c3c7f777548fbccf9509e0474d7
Max Bachhuber <max.bachhuber@bahuma.io>
2021-11-18 09:31:10 +0100

    Fix PHP code style
    Signed-off-by: Max Bachhuber <max.bachhuber@bahuma.io>

--- tests/unit/Service/BoardServiceTest.php
+++ tests/unit/Service/BoardServiceTest.php
@@ -52,8 +52,7 @@ use OCP\IUserManager;
 use PHPUnit\Framework\MockObject\MockObject;
 use Test\TestCase;
 
-class BoardServiceTest extends TestCase
-{
+class BoardServiceTest extends TestCase {
 
 	/** @var BoardService */
 	private $service;
