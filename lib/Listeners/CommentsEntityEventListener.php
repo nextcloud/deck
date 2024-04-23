006c0c3513e66d6429730426067d2364bb861d5d
Julius Härtl <jus@bitgrid.net>
2024-04-23 19:24:21 +0200

    ci: Adadpt min php version to 8.1
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ lib/Listeners/CommentsEntityEventListener.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @copyright Copyright (c) 2020 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+
+namespace OCA\Deck\Listeners;
+
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventListener;
+use OCP\IRequest;
+use OCP\Util;
+
+/** @template-implements IEventListener<Event|BeforeTemplateRenderedEvent> */
+class BeforeTemplateRenderedListener implements IEventListener {
+	private $request;
+
+	public function __construct(IRequest $request) {
+		$this->request = $request;
+	}
+
+	public function handle(Event $event): void {
+		if (!($event instanceof BeforeTemplateRenderedEvent)) {
+			return;
+		}
+
+		if (!$event->isLoggedIn()) {
+			return;
+		}
+		Util::addStyle('deck', 'deck');
+
+		$pathInfo = $this->request->getPathInfo();
+		if (str_starts_with($pathInfo, '/apps/calendar')) {
+			Util::addScript('deck', 'deck-calendar');
+		}
+
+		if (str_starts_with($pathInfo, '/call/') || str_starts_with($pathInfo, '/apps/spreed')) {
+			Util::addScript('deck', 'deck-talk');
+		}
+	}
+}
