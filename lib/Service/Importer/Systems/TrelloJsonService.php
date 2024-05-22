be263513b3bae4857bf191371398b3697e31822d
mulles <com.github@emu.lu>
2024-05-22 10:28:47 +0200

    Update TrelloJsonService.php
    removed trailing white space, I am sorry for that thx for your patience.
    
    Signed-off-by: mulles <com.github@emu.lu>

--- lib/Service/Importer/Systems/TrelloJsonService.php
+++ lib/Service/Importer/Systems/TrelloJsonService.php
@@ -344,7 +344,7 @@ class TrelloJsonService extends ABoardImportService {
 
 	private function replaceUsernames(string $text): string {
 		foreach ($this->getImportService()->getConfig('uidRelation') as $trello => $nextcloud) {
-			$text = str_replace($trello, $nextcloud, $text); 
+			$text = str_replace($trello, $nextcloud, $text);
 		}
 		return $text;
 	}
