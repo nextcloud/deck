ff0803b9817b3623196c2056829073d3b961f4df
Luka Trovic <luka@nextcloud.com>
2024-08-01 14:37:17 +0200

    fix: cypress ci issue
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,7 +17,7 @@
 import './commands.js'
 
 Cypress.on('uncaught:exception', (err) => {
-	return !err.message.includes('ResizeObserver loop limit exceeded')
+	return !err.message.includes('ResizeObserver loop limit exceeded') && !err.message.includes('ResizeObserver loop completed with undelivered notifications')
 })
 
 // Alternatively you can use CommonJS syntax:
