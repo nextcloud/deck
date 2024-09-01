75be92907704c4997f14ae06e205239486a1eee4
Julius Härtl <jus@bitgrid.net>
2024-09-01 11:58:08 +0200

    feat: Implement reference resolving for cards that have a link in the title
    Signed-off-by: Julius Härtl <jus@bitgrid.net>
    
    fix: Enrich on update
    
    Signed-off-by: Julius Knorr <jus@bitgrid.net>
    
    fix: Enrich on create
    
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- tests/integration/import/ImportExportTest.php
+++ tests/integration/import/ImportExportTest.php
@@ -135,7 +135,7 @@ class ImportExportTest extends \Test\TestCase {
 		);
 	}
 
-	public static function writeArrayStructure(string $prefix = '', array $array = [], array $skipKeyList = ['id', 'boardId', 'cardId', 'stackId', 'ETag', 'permissions', 'shared', 'version', 'done']): string {
+	public static function writeArrayStructure(string $prefix = '', array $array = [], array $skipKeyList = ['id', 'boardId', 'cardId', 'stackId', 'ETag', 'permissions', 'shared', 'version', 'done', 'referenceData']): string {
 		$output = '';
 		$arrayIsList = array_keys($array) === range(0, count($array) - 1);
 		foreach ($array as $key => $value) {
