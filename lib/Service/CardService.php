77abffd59b80c9550c727cae9bfc6db464e3aa0f
Julius Knorr <jus@bitgrid.net>
2024-12-18 19:02:09 +0100

    fixup! fixup! fixup! tmp

--- lib/Service/CardService.php
+++ lib/Service/CardService.php
@@ -237,6 +237,8 @@ class CardService {
 		$this->changeHelper->cardChanged($card->getId(), false);
 		$this->eventDispatcher->dispatchTyped(new CardCreatedEvent($card));
 
+		$card = $this->enrichCard($card);
+
 		return $card;
 	}
 
