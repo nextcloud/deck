52b40b851fe95b5d5169b3dac8a0008b9adb145b
Theo Hollweg <thollweg@mailbox.org>
2025-12-08 20:36:54 +0000

    Fix: Input for new board name appears in the location if the 'Add board' button
    Signed-off-by: Theo Hollweg <thollweg@mailbox.org>

--- src/components/navigation/AppNavigationAddBoard.vue
+++ src/components/navigation/AppNavigationAddBoard.vue
@@ -3,40 +3,42 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<NcAppNavigationItem v-if="!editing"
-		:name="t('deck', 'Add board')"
-		icon="icon-add"
-		@click.prevent.stop="startCreateBoard" />
-	<div v-else class="board-create">
-		<NcColorPicker v-model="color" class="app-navigation-entry-bullet-wrapper" :disabled="loading">
-			<div :style="{ backgroundColor: color }" class="color0 icon-colorpicker app-navigation-entry-bullet" />
-		</NcColorPicker>
-		<form @submit.prevent.stop="createBoard">
-			<NcTextField ref="inputField"
-				:disable="loading"
-				:value.sync="value"
-				:placeholder="t('deck', 'Board name')"
-				type="text"
-				required />
-			<NcButton type="tertiary"
-				:disabled="loading"
-				:title="t('deck', 'Cancel edit')"
-				@click.stop.prevent="cancelEdit">
-				<template #icon>
-					<CloseIcon :size="20" />
-				</template>
-			</NcButton>
-			<NcButton type="tertiary"
-				native-type="submit"
-				:disabled="loading"
-				:title="t('deck', 'Save board')">
-				<template #icon>
-					<CheckIcon v-if="!loading" :size="20" />
-					<NcLoadingIcon v-else :size="20" />
-				</template>
-			</NcButton>
-		</form>
-	</div>
+	<span>
+		<NcAppNavigationItem v-if="!editing"
+			:name="t('deck', 'Add board')"
+			icon="icon-add"
+			@click.prevent.stop="startCreateBoard" />
+		<div v-else class="board-create">
+			<NcColorPicker v-model="color" class="app-navigation-entry-bullet-wrapper" :disabled="loading">
+				<div :style="{ backgroundColor: color }" class="color0 icon-colorpicker app-navigation-entry-bullet" />
+			</NcColorPicker>
+			<form @submit.prevent.stop="createBoard">
+				<NcTextField ref="inputField"
+					:disable="loading"
+					:value.sync="value"
+					:placeholder="t('deck', 'Board name')"
+					type="text"
+					required />
+				<NcButton type="tertiary"
+					:disabled="loading"
+					:title="t('deck', 'Cancel edit')"
+					@click.stop.prevent="cancelEdit">
+					<template #icon>
+						<CloseIcon :size="20" />
+					</template>
+				</NcButton>
+				<NcButton type="tertiary"
+					native-type="submit"
+					:disabled="loading"
+					:title="t('deck', 'Save board')">
+					<template #icon>
+						<CheckIcon v-if="!loading" :size="20" />
+						<NcLoadingIcon v-else :size="20" />
+					</template>
+				</NcButton>
+			</form>
+		</div>
+	</span>
 </template>
 
 <script>
