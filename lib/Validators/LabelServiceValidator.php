b88498ec05a8b5694528c93ece3e2d9c8473894f
grnd-alt <git@belakkaf.net>
2026-01-07 17:00:34 +0100

    fix: validate hex color in backend
    Signed-off-by: grnd-alt <git@belakkaf.net>

--- lib/Validators/LabelServiceValidator.php
+++ lib/Validators/LabelServiceValidator.php
@@ -10,12 +10,16 @@ declare(strict_types=1);
 namespace OCA\Deck\Validators;
 
 class LabelServiceValidator extends BaseValidator {
+	protected function hex_color(string $value): bool {
+		return preg_match('/[A-Fa-f0-9]{6}/', $value) === 1;
+	}
+
 	public function rules() {
 		return [
 			'id' => ['numeric'],
 			'title' => ['not_empty', 'not_null', 'not_false', 'max:100'],
 			'boardId' => ['numeric', 'not_null'],
-			'color' => ['not_empty', 'not_null', 'not_false', 'max:6']
+			'color' => ['not_empty', 'not_null', 'not_false', 'max:6', 'hex_color']
 		];
 	}
 }
