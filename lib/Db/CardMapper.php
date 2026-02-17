024ad9f3aec0603796c02c35e90c8d009deccef4
Jacob Bühler <jacob@Mac.fritz.box>
2026-02-17 01:13:24 +0100

    fix(deck): accept null offset in CardMapper findAll for stacks API
    Signed-off-by: Jaggob <37583151+Jaggob@users.noreply.github.com>

--- lib/Db/CardMapper.php
+++ lib/Db/CardMapper.php
@@ -134,7 +134,8 @@ class CardMapper extends QBMapper implements IPermissionMapper {
 	 * @return Card[]
 	 * @throws \OCP\DB\Exception
 	 */
-	public function findAll($stackId, ?int $limit = null, int $offset = 0, int $since = -1) {
+	public function findAll($stackId, ?int $limit = null, ?int $offset = 0, int $since = -1) {
+		$offset ??= 0;
 		$qb = $this->db->getQueryBuilder();
 		$qb->select('*')
 			->from('deck_cards')
@@ -154,7 +155,8 @@ class CardMapper extends QBMapper implements IPermissionMapper {
 	 * @return array<int, null|Card[]>
 	 * @throws \OCP\DB\Exception
 	 */
-	public function findAllForStacks(array $stackIds, ?int $limit = null, int $offset = 0, int $since = -1): array {
+	public function findAllForStacks(array $stackIds, ?int $limit = null, ?int $offset = 0, int $since = -1): array {
+		$offset ??= 0;
 		$qb = $this->db->getQueryBuilder();
 		$qb->select('*')
 			->from('deck_cards')
