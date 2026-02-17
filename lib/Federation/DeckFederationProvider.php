1923cc8db1de8c1081e175f706b45f3628d56000
grnd-alt <git@belakkaf.net>
2026-02-17 17:26:31 +0100

    chore: add reuse compliance headers
    Signed-off-by: grnd-alt <git@belakkaf.net>

--- lib/Federation/DeckFederationProvider.php
+++ lib/Federation/DeckFederationProvider.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 namespace OCA\Deck\Federation;
 
 use Exception;
--- lib/Migration/Version11001Date20251020122010.php
+++ lib/Migration/Version11001Date20251020122010.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 declare(strict_types=1);
 namespace OCA\Deck\Migration;
 
