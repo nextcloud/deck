1c3eb2edff6dd526eee6f3f3744df59cf609ad6b
Anna Larch <anna@nextcloud.com>
2026-03-10 00:54:32 +0100

    fix(import): done state is not kept on importing a board Fix https://github.com/nextcloud/deck/issues/7536
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/Service/Importer/Systems/DeckJsonService.php
+++ lib/Service/Importer/Systems/DeckJsonService.php
@@ -211,6 +211,7 @@ class DeckJsonService extends ABoardImportService {
 			$boardOwner = $this->getBoard()->getOwner();
 			$card->setOwner($this->mapOwner(is_string($boardOwner) ? $boardOwner : $boardOwner->getUID()));
 			$card->setDuedate($cardSource->duedate);
+			$card->setDone($cardSource->done ?? null);
 			$cards[$cardSource->id] = $card;
 		}
 		return $cards;
