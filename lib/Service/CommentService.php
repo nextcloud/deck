749981566cae602654f508612524f6d20a103d44
grnd-alt <git@belakkaf.net>
2026-03-24 09:54:33 +0100

    fix: set max comment retrieval limit to 200
    Signed-off-by: grnd-alt <git@belakkaf.net>

--- lib/Service/CommentService.php
+++ lib/Service/CommentService.php
@@ -35,6 +35,9 @@ class CommentService {
 	}
 
 	public function list(int $cardId, int $limit = 20, int $offset = 0): DataResponse {
+		if ($limit > 200) {
+			$limit = 200;
+		}
 		$this->permissionService->checkPermission($this->cardMapper, $cardId, Acl::PERMISSION_READ);
 		$comments = $this->commentsManager->getForObject(Application::COMMENT_ENTITY_TYPE, (string)$cardId, $limit, $offset);
 		$result = [];
