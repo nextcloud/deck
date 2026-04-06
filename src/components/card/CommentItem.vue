a3b5197a5eb43c9171030fa39b869b8d5da251b2
Theo <36564257+theoholl@users.noreply.github.com>
2026-04-06 09:46:23 +0000

    Add Pinia store and update Vue components
    Signed-off-by: Theo <36564257+theoholl@users.noreply.github.com>

--- src/components/card/CommentItem.vue
+++ src/components/card/CommentItem.vue
@@ -84,6 +84,7 @@ import md5 from 'blueimp-md5'
 import relativeDate from '../../mixins/relativeDate.js'
 import ReplyIcon from 'vue-material-design-icons/ReplyOutline.vue'
 import moment from 'moment'
+import { useCommentStore } from '../../stores/comment.js'
 
 const AtMention = {
 	name: 'AtMention',
@@ -123,6 +124,10 @@ export default {
 			type: Boolean,
 			default: false,
 		},
+	},
+	setup() {
+		const commentStore = useCommentStore()
+		return { commentStore }
 	},
 	data() {
 		return {
@@ -179,7 +184,7 @@ export default {
 
 	methods: {
 		replyTo() {
-			this.$store.dispatch('setReplyTo', this.comment)
+			this.commentStore.setReplyTo(this.comment)
 		},
 		showUpdateForm() {
 			this.edit = true
@@ -197,7 +202,7 @@ export default {
 				cardId: this.comment.objectId,
 				id: this.comment.id,
 			}
-			await this.$store.dispatch('updateComment', data)
+			await this.commentStore.apiUpdateComment(data)
 			this.hideUpdateForm()
 		},
 		deleteComment() {
@@ -205,7 +210,7 @@ export default {
 				id: this.comment.id,
 				cardId: this.comment.objectId,
 			}
-			this.$store.dispatch('deleteComment', data)
+			this.commentStore.apiDeleteComment(data)
 		},
 	},
 }
