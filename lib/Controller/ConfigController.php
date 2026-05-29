abbe703a71b69d895f9494e19d00eac160992fb5
samin-z <samin.zavarkesh@gmail.com>
2026-05-29 14:33:04 +0200

    add permission check
    Signed-off-by: samin-z <samin.zavarkesh@gmail.com>

--- lib/Controller/ConfigController.php
+++ lib/Controller/ConfigController.php
@@ -9,6 +9,7 @@ namespace OCA\Deck\Controller;
 use OCA\Deck\Db\Acl;
 use OCA\Deck\Db\BoardMapper;
 use OCA\Deck\Service\ConfigService;
+use OCA\Deck\Service\PermissionService;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Http\NotFoundResponse;
 use OCP\AppFramework\OCSController;
@@ -38,6 +39,14 @@ class ConfigController extends OCSController {
 	 * @NoAdminRequired
 	 */
 	public function setValue(string $key, $value) {
+		if (preg_match('/^board:(\d+):/', $key, $matches) === 1) {
+			$this->permissionService->checkPermission(
+				$this->boardMapper,
+				(int)$matches[1],
+				Acl::PERMISSION_EDIT,
+			);
+		}
+
 		$result = $this->configService->set($key, $value);
 		if ($result === null) {
 			return new NotFoundResponse();
