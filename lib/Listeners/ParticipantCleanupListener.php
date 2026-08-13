371b66dd4af5f48a2a03fe22ed8c7925119476a9
samin-z <samin.zavarkesh@gmail.com>
2026-08-13 16:20:32 +0200

    on user leave or team delete, transfer ownership or delete board
    Signed-off-by: samin-z <samin.zavarkesh@gmail.com>

--- lib/Listeners/ParticipantCleanupListener.php
+++ lib/Listeners/ParticipantCleanupListener.php
@@ -8,43 +8,61 @@
 namespace OCA\Deck\Listeners;
 
 use OCA\Circles\Events\CircleDestroyedEvent;
+use OCA\Circles\Events\RemovingCircleMemberEvent;
+use OCA\Circles\Model\Member;
 use OCA\Deck\Db\Acl;
 use OCA\Deck\Db\AclMapper;
 use OCA\Deck\Db\AssignmentMapper;
 use OCA\Deck\Db\BoardMapper;
+use OCA\Deck\Service\TeamBoardService;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\Group\Events\GroupDeletedEvent;
 use OCP\User\Events\UserDeletedEvent;
 
-/** @template-implements IEventListener<Event|UserDeletedEvent|GroupDeletedEvent|CircleDestroyedEvent> */
+/** @template-implements IEventListener<Event|UserDeletedEvent|GroupDeletedEvent|CircleDestroyedEvent|RemovingCircleMemberEvent> */
 class ParticipantCleanupListener implements IEventListener {
-	private AclMapper $aclMapper;
-	private AssignmentMapper $assignmentMapper;
-	private BoardMapper $boardMapper;
-
-	public function __construct(AclMapper $aclMapper, AssignmentMapper $assignmentMapper, BoardMapper $boardMapper) {
-		$this->aclMapper = $aclMapper;
-		$this->assignmentMapper = $assignmentMapper;
-		$this->boardMapper = $boardMapper;
+	public function __construct(
+		private AclMapper $aclMapper,
+		private AssignmentMapper $assignmentMapper,
+		private BoardMapper $boardMapper,
+		private TeamBoardService $teamBoardService,
+	) {
 	}
 
 	public function handle(Event $event): void {
 		if ($event instanceof UserDeletedEvent) {
-			$boards = $this->boardMapper->findAllByOwner($event->getUser()->getUID());
+			$userId = $event->getUser()->getUID();
+			$transferredBoardIds = $this->teamBoardService->transferTeamBoardsFromDeletedUser($userId);
+
+			$boards = $this->boardMapper->findAllByOwner($userId);
 			foreach ($boards as $board) {
+				if (in_array($board->getId(), $transferredBoardIds, true)) {
+					continue;
+				}
 				$this->boardMapper->delete($board);
 			}
 
-			$this->cleanupByParticipant(Acl::PERMISSION_TYPE_USER, $event->getUser()->getUID());
+			$this->cleanupByParticipant(Acl::PERMISSION_TYPE_USER, $userId);
 		}
 
 		if ($event instanceof GroupDeletedEvent) {
 			$this->cleanupByParticipant(Acl::PERMISSION_TYPE_GROUP, $event->getGroup()->getGID());
 		}
 
+		if ($event instanceof RemovingCircleMemberEvent) {
+			$member = $event->getMember();
+			if ($member !== null && $member->getUserType() === Member::TYPE_USER) {
+				$this->teamBoardService->handleMemberLeftTeam(
+					$event->getCircle()->getSingleId(),
+					$member->getUserId()
+				);
+			}
+		}
+
 		if ($event instanceof CircleDestroyedEvent) {
 			$circleId = $event->getCircle()->getSingleId();
+			$this->teamBoardService->deleteBoardsAttachedToTeam($circleId);
 			$this->cleanupByParticipant(Acl::PERMISSION_TYPE_CIRCLE, $circleId);
 		}
 	}
