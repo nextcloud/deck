e4ac854985769d8ec62230716c14c75ff0ef0c61
grnd-alt <git@belakkaf.net>
2026-09-08 15:02:59 +0200

    chore(tests): replace Cypress.env
    Signed-off-by: grnd-alt <git@belakkaf.net>

--- cypress/e2e/cardFeatures.js
+++ cypress/e2e/cardFeatures.js
@@ -17,7 +17,7 @@ const auth = {
 const useModal = (useModal) => {
 	return cy.request({
 		method: 'POST',
-		url: `${Cypress.env('baseUrl')}/ocs/v2.php/apps/deck/api/v1.0/config/cardDetailsInModal?format=json`,
+		url: `${Cypress.expose('baseUrl')}/ocs/v2.php/apps/deck/api/v1.0/config/cardDetailsInModal?format=json`,
 		auth,
 		body: { value: useModal },
 	}).then((response) => {
@@ -28,7 +28,7 @@ const useModal = (useModal) => {
 const addCardsAtTop = (enabled) => {
 	return cy.request({
 		method: 'POST',
-		url: `${Cypress.env('baseUrl')}/ocs/v2.php/apps/deck/api/v1.0/config/stackAddCardAtTop?format=json`,
+		url: `${Cypress.expose('baseUrl')}/ocs/v2.php/apps/deck/api/v1.0/config/stackAddCardAtTop?format=json`,
 		auth,
 		body: { value: enabled },
 	}).then((response) => {
