11bcfe18b23465643b92a77d5fe306b3fe9273ef
Luka Trovic <luka@nextcloud.com>
2026-09-15 11:43:20 +0200

    enh: validate datetime fields
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- src/components/card/StartDateSelector.vue
+++ src/components/card/StartDateSelector.vue
@@ -83,7 +83,12 @@ export default defineComponent({
 				return this.card?.startdate ? new Date(this.card.startdate) : null
 			},
 			set(val) {
-				this.$emit('input', val ? new Date(val) : null)
+				// Prevent setting a date with a year greater than 9999, as this can cause issues with some date libraries and databases.
+				const date = val ? new Date(val) : null
+				if (date && date.getFullYear() > 9999) {
+					return
+				}
+				this.$emit('input', date)
 			},
 		},
 	},
